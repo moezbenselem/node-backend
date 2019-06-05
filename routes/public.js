166cc94f34c1e756a6ac63c20518ab84c2d2fb0b
Moez_Benselem <benselemmoez@gmail.com>
2019-06-05 15:05:37 +0100

    Update public.js

--- routes/public.js
+++ routes/public.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/events/:eventId',publicController.getEvent);
 router.get('/events', publicController.getEvents);
-router.get('/myEvents', publicController.getMyEvents);
+router.post('/myEvents', publicController.getMyEvents);
 
 router.post('/myEvents', publicController.postParticipate);
 router.get('/remove/:eventId', publicController.postRemove);
